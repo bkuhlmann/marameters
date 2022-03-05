023d34fb120b70cbf17f22b8b1056c30fe7e6d44
Brooke Kuhlmann <brooke@alchemists.io>
2022-03-05 14:20:16 -0700

    Added transformer
    Necessary to provide a common function which can be injected by all
    parameter objects. This will reduce duplication across multiple
    objects.
    
    Testing, for now, will be delegated to the individual objects that use
    this function.

--- lib/marameters.rb
+++ lib/marameters.rb
@@ -6,4 +6,8 @@ Zeitwerk::Loader.for_gem.setup
 
 # Main namespace.
 module Marameters
+  TRANSFORMER = lambda do |parameters, pattern|
+    parameters.select { |kind, _name| pattern.include? kind }
+              .reduce({}) { |collection, (kind, name)| collection.merge kind => name }
+  end
 end
