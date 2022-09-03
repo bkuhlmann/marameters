c129c083242c3b94bd512b8afa608e34e8486b28
Brooke Kuhlmann <brooke@alchemists.io>
2022-09-03 08:01:13 -0600

    Removed kinds constant
    When originally added (e4137d43d50e), it seemed like a good constant
    to have for convenience but the `nokey` kind throws a wrinkle in this
    design so am opting to remove since the other classes in this gem with
    kinds of parameters much better.

--- lib/marameters.rb
+++ lib/marameters.rb
@@ -6,8 +6,6 @@ Zeitwerk::Loader.for_gem.setup
 
 # Main namespace.
 module Marameters
-  KINDS = %i[req opt rest keyreq key keyrest block].freeze
-
   def self.categorize(parameters, arguments) = Categorizer.new(parameters).call(arguments)
 
   def self.of(...) = Probe.of(...)
