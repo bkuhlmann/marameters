237868970748b9342e1fc9fe68ba122f42e7eda2
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-10 17:37:22 -0600

    Removed defaulter passthrough in favor of syntax parsing
    Necessary to improve security by using the VM to acquire the source code from the proc/lambda since we don't need to evaluate but only extract the _literal_ contents for use when building method signatures later.
    
    The source node is always `2` for procs/lambdas. This is unfortunate since `2` ends up being a magic number but, for this use case, I opted to stick with the magic number since the second node is always the source (body) of the proc/lambda.
    
    Procs/lambdas, when used as default values, are not meant to be defined with parameters because they'll never be called. To prevent parameter use, this will also check for procs and immediately fail so folks can correct behavior early and quickly. The only way a `Proc` will appear as a value is if the `Signature` finds one with a non-zero arity.
    
    This also eliminates the need to use the `passthrough` which was awkward and unsafe to begin with. Now procs can serve as a delivery mechanism without having folks learn new syntax.
    
    Milestone: major

--- lib/marameters/defaulter.rb
+++ lib/marameters/defaulter.rb
@@ -2,11 +2,12 @@
 
 module Marameters
   # Computes a method parameter's default value.
-  Defaulter = lambda do |value, passthrough: "*"|
+  Defaulter = lambda do |value|
     case value
-      when /\A#{Regexp.escape passthrough}/ then value.delete_prefix passthrough
+      when Proc then fail(ArgumentError, "Avoid using parameters for proc/lambda defaults.")
       when String then value.dump
       when Symbol then value.inspect
+      when RubyVM::AbstractSyntaxTree::Node then value.children[2].source
       when nil then "nil"
       else value
     end
