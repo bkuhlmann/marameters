1b21b4104558e6886d0c6151dfbf9703404ba4e3
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-12 09:38:24 -0600

    Added signature argument forwarding
    Necessary to provide argument forwarding in the following situations:
    
    * *Argument Forwarding*: Standard argument forwarding (i.e. `+...+`) that is provided for convenience when needing to build a method signature that will forward all arguments to a custom method.
    * *Of*: Specific to method inheritance where you need to compose your method signature to be a part of the parameters defined by the super method. This combines the parameters from the super method with the sub parameters to produce a method signature that properly forwards to the super.
    * *For Super*: This is specific to method inheritance as well and works in conjunction with the `.of` method mentioned above. This allows you to produce a method signature compatible with forwarding to `super`.
    
    RuboCop was updated to ignore the missing `else` in the case statement because `nil` is valid for unknown kinds which are filter mapped out.
    
    Milestone: minor

--- lib/marameters/signature.rb
+++ lib/marameters/signature.rb
@@ -3,19 +3,44 @@
 module Marameters
   # Builds a method's parameter signature.
   class Signature
+    def self.of super_params,
+                sub_params,
+                aids: {anonymizer: Signatures::Anonymizer, reducer: Signatures::Reducer}
+
+      aids.fetch(:anonymizer)
+          .call(*super_params)
+          .union(sub_params)
+          .then { |union| aids.fetch(:reducer).call union }
+          .sort_by! { |(kind, *)| KINDS.index kind }
+          .then { |forwards| new(*forwards) }
+    end
+
     def initialize *parameters, parser: RubyVM::AbstractSyntaxTree, builder: Signatures::Builder.new
       @parameters = parameters.all?(Array) ? parameters : [parameters]
       @parser = parser
       @builder = builder
     end
 
-    def to_s = build.join ", "
+    def for_super = build_for_super.join ", "
+
+    def to_s = parameters == [[:all]] ? "..." : build.join(", ")
 
     alias to_str to_s
 
     private
 
     attr_reader :parameters, :parser, :builder
+
+    def build_for_super positionals = CATEGORIES.positionals
+      parameters.filter_map do |kind, name|
+        case kind
+          when *positionals then name
+          when :rest then "*"
+          when :keyrest then "**"
+          when :block then "&"
+        end
+      end
+    end
 
     def build
       parameters.reduce [] do |signature, (kind, name, default)|
