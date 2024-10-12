443811ed8c07137efddae1efc927799cf37b5175
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-12 12:53:56 -0600

    Updated splat struct as forward model
    The use of `Splat` as a constant didn't entirely make sense so am now calling this a _forward_ model for capturing all arguments that can be forwarded.
    
    Milestone: major

--- lib/marameters/splat.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Marameters
-  # Captures arguments, by category, for message splatting.
-  Splat = Struct.new :positionals, :keywords, :block do
-    def initialize(positionals: [], keywords: {}, block: nil) = super
-  end
-end
