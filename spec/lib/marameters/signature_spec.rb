c4a808a7c98ecb383617da732644afdcf9c51a07
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-12 09:38:24 -0600

    Added signature argument forwarding
    Necessary to provide argument forwarding in the following situations:
    
    * *Argument Forwarding*: Standard argument forwarding (i.e. `+...+`) that is provided for convenience when needing to build a method signature that will forward all arguments to a custom method.
    * *Of*: Specific to method inheritance where you need to compose your method signature to be a part of the parameters defined by the super method. This combines the parameters from the super method with the sub parameters to produce a method signature that properly forwards to the super.
    * *For Super*: This is specific to method inheritance as well and works in conjunction with the `.of` method mentioned above. This allows you to produce a method signature compatible with forwarding to `super`.
    
    RuboCop was updated to ignore the missing `else` in the case statement because `nil` is valid for unknown kinds which are filter mapped out.
    
    Milestone: minor

--- spec/lib/marameters/signature_spec.rb
+++ spec/lib/marameters/signature_spec.rb
@@ -5,7 +5,256 @@ require "spec_helper"
 RSpec.describe Marameters::Signature do
   subject(:signature) { described_class.new(*parameters) }
 
+  describe ".of" do
+    let(:super_params) { [%i[req one]] }
+    let(:sub_params) { [%i[req child]] }
+
+    it "answers empty string when super and sub parameters are empty" do
+      expect(described_class.of([], []).to_s).to eq("")
+    end
+
+    it "answers required positional (super)" do
+      signature = described_class.of([%i[req test]], sub_params).to_s
+      expect(signature).to eq("test, child")
+    end
+
+    it "answers required positional (sub)" do
+      signature = described_class.of(super_params, [%i[req test]]).to_s
+      expect(signature).to eq("one, test")
+    end
+
+    it "answers optional positional (super)" do
+      signature = described_class.of([%i[opt test]], sub_params).to_s
+      expect(signature).to eq("child, test = nil")
+    end
+
+    it "answers optional positional (sub)" do
+      signature = described_class.of(super_params, [%i[opt test]]).to_s
+      expect(signature).to eq("one, test = nil")
+    end
+
+    it "answers named single splat (super)" do
+      signature = described_class.of([%i[rest test]], sub_params).to_s
+      expect(signature).to eq("child, *")
+    end
+
+    it "answers named single splat (sub)" do
+      signature = described_class.of(super_params, [%i[rest test]]).to_s
+      expect(signature).to eq("one, *test")
+    end
+
+    it "answers anonymous single splat (super)" do
+      signature = described_class.of([[:rest]], sub_params).to_s
+      expect(signature).to eq("child, *")
+    end
+
+    it "answers anonymous single splat (sub)" do
+      signature = described_class.of(super_params, [[:rest]]).to_s
+      expect(signature).to eq("one, *")
+    end
+
+    it "answers required keyword (super)" do
+      signature = described_class.of([%i[keyreq test]], sub_params).to_s
+      expect(signature).to eq("child, **")
+    end
+
+    it "answers required keyword (sub)" do
+      signature = described_class.of(super_params, [%i[keyreq test]]).to_s
+      expect(signature).to eq("one, test:")
+    end
+
+    it "answers optional keyword (super)" do
+      signature = described_class.of([%i[key test]], sub_params).to_s
+      expect(signature).to eq("child, **")
+    end
+
+    it "answers optional keyword (sub)" do
+      signature = described_class.of(super_params, [%i[key test]]).to_s
+      expect(signature).to eq("one, test: nil")
+    end
+
+    it "answers named double splat (super)" do
+      signature = described_class.of([%i[keyrest test]], sub_params).to_s
+      expect(signature).to eq("child, **")
+    end
+
+    it "answers named double splat (sub)" do
+      signature = described_class.of(super_params, [%i[keyrest test]]).to_s
+      expect(signature).to eq("one, **test")
+    end
+
+    it "answers anonymous double splat (super)" do
+      signature = described_class.of([[:keyrest]], sub_params).to_s
+      expect(signature).to eq("child, **")
+    end
+
+    it "answers anonymous double splat (sub)" do
+      signature = described_class.of(super_params, [[:keyrest]]).to_s
+      expect(signature).to eq("one, **")
+    end
+
+    it "answers named block (super)" do
+      signature = described_class.of([%i[block test]], sub_params).to_s
+      expect(signature).to eq("child, &")
+    end
+
+    it "answers named block (sub)" do
+      signature = described_class.of(super_params, [%i[block test]]).to_s
+      expect(signature).to eq("one, &test")
+    end
+
+    it "answers anonymous block (super)" do
+      signature = described_class.of([[:block]], sub_params).to_s
+      expect(signature).to eq("child, &")
+    end
+
+    it "answers anonymous block (sub)" do
+      signature = described_class.of(super_params, [[:block]]).to_s
+      expect(signature).to eq("one, &")
+    end
+
+    it "includes defaults" do
+      signature = described_class.of super_params, [[:opt, :two, 2], [:key, :three, 3]]
+      expect(signature.to_s).to eq("one, two = 2, three: 3")
+    end
+
+    it "removes duplicates" do
+      signature = described_class.of super_params, [%i[req one], %i[req two], %i[req two]]
+      expect(signature.to_s).to eq("one, two")
+    end
+
+    it "overrides duplicates" do
+      signature = described_class.of [%i[rest test]], [[:rest]]
+      expect(signature.to_s).to eq("*")
+    end
+
+    it "overrides duplicates with defaults" do
+      signature = described_class.of super_params, [[:opt, :two, 1], [:opt, :two, 2]]
+      expect(signature.to_s).to eq("one, two = 2")
+    end
+
+    it "answers sorted parameters" do
+      signature = described_class.of super_params, [[:block], [:keyrest], [:rest], %i[opt two]]
+      expect(signature.to_s).to eq("one, two = nil, *, **, &")
+    end
+
+    it "answers unique parameters" do
+      signature = described_class.of [%i[req one], %i[rest one], %i[keyrest two], %i[block three]],
+                                     [%i[req one], [:rest], [:keyrest], [:block]]
+
+      expect(signature.to_s).to eq("one, *, **, &")
+    end
+
+    it "answers all parameters" do
+      all = [
+        %i[req one],
+        %i[opt two],
+        %i[rest three],
+        %i[keyreq four],
+        %i[key five],
+        %i[keyrest six],
+        %i[block seven]
+      ]
+
+      signature = described_class.of all, all
+
+      expect(signature.to_s).to eq("one, two = nil, *, four:, five: nil, **, &")
+    end
+  end
+
+  describe "#for_super" do
+    context "with all kinds" do
+      let :parameters do
+        [
+          %i[req one],
+          %i[opt two],
+          %i[rest three],
+          %i[keyreq four],
+          %i[key five],
+          %i[keyrest six],
+          %i[block seven]
+        ]
+      end
+
+      it "answers named positionals and anonymous forwards" do
+        expect(signature.for_super).to eq("one, two, *, four:, five:, **, &")
+      end
+    end
+
+    context "with only required positional" do
+      let(:parameters) { %i[req test] }
+
+      it "answers name" do
+        expect(signature.for_super).to eq("test")
+      end
+    end
+
+    context "with only optional positional" do
+      let(:parameters) { %i[opt test] }
+
+      it "answers name" do
+        expect(signature.for_super).to eq("test")
+      end
+    end
+
+    context "with only single splat" do
+      let(:parameters) { [:rest] }
+
+      it "answers single splat" do
+        expect(signature.for_super).to eq("*")
+      end
+    end
+
+    context "with only required keyword" do
+      let(:parameters) { %i[keyreq four] }
+
+      it "answers key" do
+        expect(signature.for_super).to eq("four:")
+      end
+    end
+
+    context "with only optional keyword" do
+      let(:parameters) { %i[key five] }
+
+      it "answers key" do
+        expect(signature.for_super).to eq("five:")
+      end
+    end
+
+    context "with only double splat" do
+      let(:parameters) { [:keyrest] }
+
+      it "answers double splat" do
+        expect(signature.for_super).to eq("**")
+      end
+    end
+
+    context "with only block" do
+      let(:parameters) { [:block] }
+
+      it "answers ampersand" do
+        expect(signature.for_super).to eq("&")
+      end
+    end
+
+    context "with no kinds" do
+      subject(:signature) { described_class.new }
+
+      it "answers empty string" do
+        expect(signature.for_super).to eq("")
+      end
+    end
+  end
+
   shared_examples "a string" do |method|
+    context "with argument forwarding" do
+      subject(:signature) { described_class.new :all }
+
+      it "answers parameter" do
+        expect(signature.public_send(method)).to eq("...")
+      end
+    end
+
     context "with required positional" do
       let(:parameters) { %i[req one] }
 
@@ -56,7 +305,7 @@ RSpec.describe Marameters::Signature do
     end
 
     context "with bare single splat" do
-      let(:parameters) { [:rest, nil] }
+      let(:parameters) { [:rest] }
 
       it "answers parameter" do
         expect(signature.public_send(method)).to eq("*")
@@ -121,7 +370,7 @@ RSpec.describe Marameters::Signature do
     end
 
     context "with bare double splat" do
-      let(:parameters) { [:keyrest, nil] }
+      let(:parameters) { [:keyrest] }
 
       it "answers parameter" do
         expect(signature.public_send(method)).to eq("**")
@@ -137,7 +386,7 @@ RSpec.describe Marameters::Signature do
     end
 
     context "with bare block" do
-      let(:parameters) { [:block, nil] }
+      let(:parameters) { [:block] }
 
       it "answers parameter" do
         expect(signature.public_send(method)).to eq("&")
@@ -149,6 +398,14 @@ RSpec.describe Marameters::Signature do
 
       it "answers parameter" do
         expect(signature.public_send(method)).to eq("&seven")
+      end
+    end
+
+    context "with no parameters" do
+      subject(:signature) { described_class.new }
+
+      it "answers empty string" do
+        expect(signature.public_send(method)).to eq("")
       end
     end
 
