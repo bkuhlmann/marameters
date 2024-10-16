5fd0d44f4ad3f23eede89cc45ca2d88092ac25cd
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-16 12:02:14 -0600

    Refactored signature specific objects to signatures namespace
    Necessary to improve organization and clarify these are supporting objects for building method signatures.
    
    Milestone: major

---
+++ lib/marameters/signatures/defaulter.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Marameters
+  module Signatures
+    # Computes a method parameter's default value.
+    Defaulter = lambda do |value|
+      case value
+        when Proc then fail(ArgumentError, "Avoid using parameters for proc/lambda defaults.")
+        when String then value.dump
+        when Symbol then value.inspect
+        when RubyVM::AbstractSyntaxTree::Node then value.children[2].source
+        when nil then "nil"
+        else value
+      end
+    end
+  end
+end
