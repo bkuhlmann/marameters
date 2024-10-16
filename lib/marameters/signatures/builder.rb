c5bc049fd19248f30cd1bb6ed306bb0a86253319
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-16 12:02:14 -0600

    Refactored signature specific objects to signatures namespace
    Necessary to improve organization and clarify these are supporting objects for building method signatures.
    
    Milestone: major

---
+++ lib/marameters/signatures/builder.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Marameters
+  module Signatures
+    # Builds a single parameter of a method's parameter signature.
+    class Builder
+      def initialize defaulter: Defaulter
+        @defaulter = defaulter
+      end
+
+      def call kind, name = nil, default: nil
+        case kind
+          when :req then name
+          when :opt then "#{name} = #{defaulter.call default}"
+          when :rest then "*#{name}"
+          when :nokey then "**nil"
+          when :keyreq then "#{name}:"
+          when :key then "#{name}: #{defaulter.call default}"
+          when :keyrest then "**#{name}"
+          when :block then "&#{name}"
+          else fail ArgumentError, "Wrong kind (#{kind}), name (#{name}), or default (#{default})."
+        end
+      end
+
+      private
+
+      attr_reader :defaulter
+    end
+  end
+end
