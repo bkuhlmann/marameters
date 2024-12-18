4a56055c31b4bd4e28d76070976639b7706f2451
Brooke Kuhlmann <brooke@alchemists.io>
2024-12-18 11:13:05 -0700

    Added signature sourcer
    Necessary to acquire the source code of a `Proc`. This will soon be used so default values for parameters can be wrapped in a `Proc` for lazy evaluation of complex objects.
    
    There are situations -- like when using IRB, `eval`, C code, and so forth -- where the source path isn't a path and will answer a string wrapped in parenthesis instead. This accounts for those situations by ensuring the path starts with a file separator and answer `nil` if otherwise so the generated signature remains syntactically correct.
    
    Milestone: minor

---
+++ lib/marameters/signatures/sourcer.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Marameters
+  module Signatures
+    # Acquires a proc's source code.
+    class Sourcer
+      PATTERN = /
+        proc          # Proc statement.
+        \s*           # Optional space.
+        \{            # Block open.
+        (?<body>.*?)  # Source code body.
+        \}            # Block close.
+      /x
+
+      def initialize pattern: PATTERN, offset: 1, io: File
+        @pattern = pattern
+        @offset = offset
+        @io = io
+        @fallback = "nil"
+      end
+
+      def call function
+        path, line_number = function.source_location
+
+        return fallback unless path && line_number && path.start_with?(File::SEPARATOR)
+
+        io.open(path) { |body| pluck body, line_number }
+      end
+
+      private
+
+      attr_reader :pattern, :offset, :io, :fallback
+
+      def pluck body, line_number
+        body.each_line
+            .with_index
+            .find { |_line, index| index + offset == line_number }
+            .first
+            .then { |line| line.match?(pattern) ? line.match(pattern)[:body].strip : fallback }
+      end
+    end
+  end
+end
---
+++ spec/lib/marameters/signatures/sourcer_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Marameters::Signatures::Sourcer do
+  subject(:sourcer) { described_class.new }
+
+  describe "#call" do
+    it "answers source code when a proc" do
+      function = proc { "test" }
+      expect(sourcer.call(function)).to eq(%("test"))
+    end
+
+    it "answers nil when a lambda" do
+      function = -> { "test" }
+      expect(sourcer.call(function)).to eq("nil")
+    end
+
+    it "answers nil when path starts with parentheses" do
+      function = Object.method :new
+      allow(function).to receive(:source_location).and_return ["(irb)", 1]
+
+      expect(sourcer.call(function)).to eq("nil")
+    end
+
+    it "answers nil when source can't be found" do
+      function = Object.method :new
+      expect(sourcer.call(function)).to eq("nil")
+    end
+  end
+end
